6f98169d00af57860488260bf8dd6b88f0161742
Sumit Kumar <105169795+sumitk289@users.noreply.github.com>
2023-02-15 22:13:06 +0530

    Add files via upload

---
+++ patterns/InvertedHalfPyramidWithNumbers.java
@@ -0,0 +1,18 @@
+package com.patterns;
+
+public class InvertedHalfPyramidWithNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		
+		for (int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i+1; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
